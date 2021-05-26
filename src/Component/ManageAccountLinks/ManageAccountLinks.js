8ef83ba756f85145f386b59fcfffd06cac6ed8d0
Annamalai <annamalai.arumugam@zhagaramss.com>
2021-05-26 12:27:47 +0530

    updated carlistpage stylesheet

--- src/Component/ManageAccountLinks/ManageAccountLinks.js
+++ src/Component/ManageAccountLinks/ManageAccountLinks.js
@@ -8,7 +8,7 @@ const ManageAccountLinks = () => {
         <div className="mgaccountuserlinks">
             <div className="userlinks">
                 <ul>
-                    <li><img src={process.env.PUBLIC_URL +"/images/sports-car.svg"} className="img-fluid" alt=""/><a href="JavaScript:void(0)"  onClick={()=>history.push('/manageaccount')} >Account</a></li>
+                    <li className="active "><img src={process.env.PUBLIC_URL +"/images/sports-car.svg"} className="img-fluid" alt=""/><a href="JavaScript:void(0)" onClick={()=>history.push('/manageaccount')} >Account</a></li>
                     <li><img src={process.env.PUBLIC_URL +"/images/Icon awesome-user.svg"} className="img-fluid" alt=""/><a href="JavaScript:void(0)"  onClick={()=>history.push('/myprofile')} >Myprofile</a></li>
                     <li><img src={process.env.PUBLIC_URL +"/images/Icon awesome-bell.svg"} className="img-fluid" alt=""/><a href="JavaScript:void(0)"  onClick={()=>history.push('/notification')} >Notification</a></li>
                     <li><img src={process.env.PUBLIC_URL +"/images/dollar-symbol.svg"} className="img-fluid" alt=""/><a href="JavaScript:void(0)"  onClick={()=>history.push('/paymentinfo')} >Payment</a></li>
