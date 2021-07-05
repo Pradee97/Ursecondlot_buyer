50c203d262498a253da7ad001ff43da402f24bd6
ursecondlot <ursecondlot@gmail.com>
2021-07-05 10:40:01 -0400

    validations for changepwd,forgetpwd,forgtpwrdeml

--- src/Pages/ForgotPassword/ForgotPassword.js
+++ src/Pages/ForgotPassword/ForgotPassword.js
@@ -4,9 +4,11 @@ import API from "../../Services/BaseService";
 import CommonPopup from '../../Component/CommonPopup/CommonPopup';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { useForm } from "react-hook-form";
 const ForgotPassword = () => {
     const history = useHistory();
     const eye = <FontAwesomeIcon icon={faEye} />;
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const [password, setPassword] = useState("");
     const [newPassword, setNewPassword] = useState("");
     const [popupTitle, setPopupTitle] = useState("");
@@ -30,7 +32,12 @@ const ForgotPassword = () => {
       }
     let value=window.location.href.split("id=");
     const changehandleSubmit = (event) => {
-        event.preventDefault();
+        //event.preventDefault();
+        if(password !== newPassword){
+            setErrorMessage("New password and Confirm password doesn't match ! ");
+        }
+        
+        else{
         let request = {
             password:password,
             user_id:value[1]
@@ -65,7 +72,7 @@ const ForgotPassword = () => {
                 setPopupActionType("close");
                 setPopupActionValue("close");
             });
-        
+        }
     }
     const togglePopup = () => {
         setIsOpen(!isOpen);
@@ -78,21 +85,45 @@ const ForgotPassword = () => {
                     <div className="col-lg-4  loginBlock">
                         <button className="back-btn-paymentform backBtn" onClick={() => history.push("/login")}><i className="icofont-arrow-left"></i> Back</button>
                         <div className="col-lg-12 card">
-                            <form className="registrationform" onSubmit={changehandleSubmit} >
+                            <form className="registrationform" onSubmit={handleSubmit(changehandleSubmit)} >
                                 <h2 className="title">Forgot Password</h2>
                                 <p className="error-message">{errorMessage}</p>
                                 <div className="row changePassblock">
 
                                     <div className="col-sm-12 form-group">
                                         <div className="tbox">
-                                            <input type={showPwd?"text":"password"} id="password" className="textbox" placeholder="" required onChange={(e) => setPassword(e.target.value)} />
+                                            <input type={showPwd?"text":"password"} id="password" className="textbox" placeholder="" {...register("password", {
+                                    required: "Password is required.",
+                                    pattern: {
+                                      value: /(^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,}))/,
+                                      message: "Password must have minimum of 8 characters with the combination of upper ,lower case letters , number and a special character"
+                                    },
+                                    minLength: {
+                                      value: 8,
+                                      message: "Password must have minimum 8 characters"
+                                    }
+                                  })} onChange={(e) => setPassword(e.target.value)} />
                                             <label htmlFor="password" className={password != "" ? "input-has-value" : ""}>Password</label><i htmlFor ="password" className="passwordeye"  onClick={togglepwd}>{eye}</i>
+                                            <p className="form-input-error">{errors.password?.message}</p>
+                                
                                         </div>
                                     </div>
                                     <div className="col-sm-12 form-group">
                                         <div className="tbox">
-                                            <input type={showPwds?"text":"password"} id="newPassword" className="textbox" placeholder="" required onChange={(e) => setNewPassword(e.target.value)} />
+                                            <input type={showPwds?"text":"password"} id="newPassword" className="textbox" placeholder="" {...register("newPassword", {
+                                    required: "Confirm password is required.",
+                                    pattern: {
+                                      value: /(^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,}))/,
+                                      message: "Password must have minimum of 8 characters with the combination of upper ,lower case letters , number and a special character"
+                                    },
+                                    minLength: {
+                                      value: 8,
+                                      message: "Password must have minimum 8 characters"
+                                    }
+                                  })} onChange={(e) => setNewPassword(e.target.value)} />
                                             <label htmlFor="newPassword" className={newPassword != "" ? "input-has-value" : ""}>Confirm Password</label><i htmlFor ="newPassword" className="passwordeye"  onClick={togglepwds}>{eye}</i>
+                                            <p className="form-input-error">{errors.newPassword?.message}</p>
+                                            <p className="form-input-error">{errorMessage}</p>
                                         </div>
                                     </div>
 
