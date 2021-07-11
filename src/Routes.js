f311da8fd467331530808e50b2dc333c8a2626e9
muthu <muthurajaguru2013@gmail.com>
2021-07-11 22:11:37 +0530

    transport page

--- src/Routes.js
+++ src/Routes.js
@@ -45,7 +45,8 @@ import ForgotPasswordEmail from './Pages/ForgotPassword/ForgotPasswordEmail';
 import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
 import ForgotEmail from './Pages/ForgotPassword/ForgotEmail';
 import Favoritelist from './Pages/ManageAccount/FavoriteList';
-import Search from './Pages/ManageAccount/Search'
+import Search from './Pages/Search';
+import Transport from './Pages/Transport';
 function AppRouter() {
 
   const timeout = 900000;
@@ -106,7 +107,7 @@ function AppRouter() {
               <Route  path="/carList" component={CarList}/>
               <Route  path="/search" component={Search}/>
               <Route  path="/mybids" component={Inprogress}/>
-              <Route  path="/transport" component={Inprogress}/>
+              <Route  path="/transport" component={Transport}/>
               <Route  path="/manageaccount" component={ManageAccount}/>
               <Route  path="/lotfee" component={LotFee}/>
               <Route  path="/notification" component={Notification}/>
