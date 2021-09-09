101f5195b07ecc03f593901d57f189916e2b4aa4
muthu <muthurajaguru2013@gmail.com>
2021-09-09 21:44:18 +0530

    history and cart page

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,17 +1,80 @@
-import React from 'react';
-import { useState } from 'react';
-import vehicles from '../assets/img/vehicles.jpg'
-import appstore from '../assets/img/appstore.png';
-import googleplay from '../assets/img/googleplay.png';
-import Checkout from '../Pages/Checkout';
-import Popup from '../Component/Popup/Popup';
+import React, {  useState, useEffect } from 'react';
+import ls from 'local-storage';
+import API from "../../Services/BaseService";
+import vehicles from '../../assets/img/vehicles.jpg'
+import appstore from '../../assets/img/appstore.png';
+import googleplay from '../../assets/img/googleplay.png';
+import Checkout from '../Checkout';
+import Popup from '../../Component/Popup/Popup';
 
 const Cart = () => {
 
+    const userDetails=ls.get('userDetails');
     const [isOpen, setIsOpen] = useState(false);
+    const [cartDetail,setCartDetail] = useState();
 
+    const [highBid,setHighBid] = useState();
+    const [feeDetails, setFeeDetails] = useState("");
+    const [total,setTotal] = useState(0);
+    const mytotal = [0]
     const togglePopup = () => {
         setIsOpen(!isOpen);
+    }
+
+    const cartDetails = () =>{
+
+        let request = {
+            buyer_dealer_id :userDetails.buyer_dealer_id,
+        }
+
+        API.post("cartDetails/condition", request).then(response=>{
+
+            console.log("cart check the value", response.data.data)
+            setCartDetail(response.data.data)
+        });
+
+    }
+
+    useEffect (() =>{
+        cartDetails()
+    }, []);
+
+
+    async function fetchBuyerFees() {
+        let request = {
+            type: "Buyer"
+        };
+        const state = API.post('fees/condition', request);
+        state.then(res => {
+            console.log("res", res)
+            setFeeDetails(res.data.data);
+          
+        })
+            .catch(err => { console.log(err); });
+    }
+    useEffect(() => {
+        fetchBuyerFees();
+    }, []);
+
+    const getFeeDetails = (maxPrice) =>{
+       
+        return feeDetails.length > 0 ? feeDetails
+            .filter((data)=> 
+           
+            {
+                const range = data.fee_price.replaceAll('$',"").split("-")
+               
+                if(range[1]!=="up"){
+                   
+                    return Number(range[0]) <= Number(maxPrice) && Number(maxPrice)  <= Number(range[1]) 
+                }
+                else{
+                    return Number(range[0]) <= Number(maxPrice) 
+                }
+
+                } 
+                )[0]?.fee || 0
+            : 0
     }
 
     return (
@@ -30,10 +93,37 @@ const Cart = () => {
             <div class="row content">
                 <div class="col-lg-8 col-md-8">
                     <div class="vehiclesheads">
-                        <h2>Number of Vehicles- 2<span>Total amount- <b>$4,600</b></span></h2>
-                    </div>
-                    
+                    <h2>Number of Vehicles- 2<span>Total amount- <b>$ 1000</b></span></h2>
+                       
+                </div>
+                {cartDetail?.length>0?cartDetail
+                            .map((cartDetail) =>    
                     <div class="vehiclesheadspaydetails mt-4">
+                        <div class="row">					
+                            <div class="vehiclepaycheckbox col-lg-12">
+                                <div class="form-group input-group">
+                                    <input type="checkbox" id="vehiclepayseparat"/><label for="vehiclepayseparat">You Want To Pay Separately</label>
+                                </div>
+                            </div>					
+                        
+                            <div class="vehicleimgleft col-lg-4">
+                                <img src={cartDetail.image}/>
+                            </div>
+                            <div class="vehicleimgright col-lg-8">
+                                <h3>{cartDetail.make} ({cartDetail.model}) <span>$ {cartDetail.max_price}</span></h3>
+                                <h4>Buy Fee <span>$ {getFeeDetails(cartDetail.max_price)}</span></h4>
+                                <div class="form-group input-group ">
+                                    <input type="checkbox" id="paytransportation" value={cartDetail.transportation == 'yes' ? 'no' : 'yes'}   checked={cartDetail.transportation== 'yes' ? true : false}/><label for="paytransportation">Transportation</label><span>$ {cartDetail.transportation === 'yes' ? cartDetail.transportation_charge : 0}</span>
+                                </div>
+                                <div class="vehiclerighttotal">
+                                    <h3>Total amount <span>$ {(Number(cartDetail.max_price) || 0) +  Number(cartDetail.transportation === 'yes' ? cartDetail.transportation_charge : 0) + Number(getFeeDetails(cartDetail.max_price))}</span></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>)
+                    :""}
+                    
+                    {/* <div class="vehiclesheadspaydetails mt-4">
                         <div class="row">					
                             <div class="vehiclepaycheckbox col-lg-12">
                                 <div class="form-group input-group">
@@ -46,7 +136,7 @@ const Cart = () => {
                             </div>
                             <div class="vehicleimgright col-lg-8">
                                 <h3>Honda Amaze (2014 model) <span>$1900</span></h3>
-                                <h4>Buy Fee <span>$100</span></h4>
+                                <h4>Buy Fee <span>$ {getFeeDetails()}</span></h4>
                                 <div class="form-group input-group ">
                                     <input type="checkbox" id="paytransportation"/><label for="paytransportation">Transportation</label><span>$300</span>
                                 </div>
@@ -55,31 +145,7 @@ const Cart = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    
-                    <div class="vehiclesheadspaydetails mt-4">
-                        <div class="row">					
-                            <div class="vehiclepaycheckbox col-lg-12">
-                                <div class="form-group input-group">
-                                    <input type="checkbox" id="vehiclepayseparat"/><label for="vehiclepayseparat">You Want To Pay Separately</label>
-                                </div>
-                            </div>					
-                        
-                            <div class="vehicleimgleft col-lg-4">
-                                <img src={vehicles}/>
-                            </div>
-                            <div class="vehicleimgright col-lg-8">
-                                <h3>Honda Amaze (2014 model) <span>$1900</span></h3>
-                                <h4>Buy Fee <span>$100</span></h4>
-                                <div class="form-group input-group ">
-                                    <input type="checkbox" id="paytransportation"/><label for="paytransportation">Transportation</label><span>$300</span>
-                                </div>
-                                <div class="vehiclerighttotal">
-                                    <h3>Total amount <span>$2,300</span></h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    </div> */}
                 </div>
                 <div class="col-lg-4 col-md-8">
                     <div class="vehicletotal">
@@ -89,6 +155,9 @@ const Cart = () => {
                         <div class="input-group">
                             <select id="vehicleselect"  class="form-control custom-select browser-default">
                                 <option value="Select">select</option>
+                                <option value="Select">ACH</option>
+                                <option value="Select">Floor</option>
+                                <option value="Select">Credit Card</option>
                             </select>
                         </div>
                         <div class="vehicletotalbtns"> 
--- src/Pages/ManageAccount/History.js
+++ src/Pages/ManageAccount/History.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React , {  useState, useEffect } from 'react';
+import { useHistory} from "react-router-dom";
+import ls from 'local-storage';
+import API from "../../Services/BaseService";
 import cars01 from '../../assets/img/cars01.png';
 import speedometer from '../../assets/img/speedometer.svg';
 import gasolinePump from '../../assets/img/gasolinePump.svg';
@@ -6,6 +9,75 @@ import appstore from '../../assets/img/appstore.png';
 import googleplay from '../../assets/img/googleplay.png';
 
 const History = () => {
+
+  const history = useHistory();
+  const userDetails=ls.get('userDetails');
+  const [historyDetail,setHistoryDetail] = useState();
+  const [feeDetails, setFeeDetails] = useState("");
+
+  const redirecttoInspection=(pathid)=>{
+    //   history.push("/Inspection/"+pathid);
+    history.push({
+      pathname: "/Inspection",
+      state: {id:pathid},
+      });
+    }
+
+  const historyDetails = () =>{
+
+    let request = {
+        buyer_dealer_id :userDetails.buyer_dealer_id,
+    }
+
+    API.post("historyDetails/condition", request).then(response=>{
+
+        console.log("history check the value", response.data.data)
+        setHistoryDetail(response.data.data)
+    });
+
+}
+
+useEffect (() =>{
+  historyDetails()
+}, []);
+
+async function fetchBuyerFees() {
+  let request = {
+      type: "Buyer"
+  };
+  const state = API.post('fees/condition', request);
+  state.then(res => {
+      console.log("res", res)
+      setFeeDetails(res.data.data);
+    
+  })
+      .catch(err => { console.log(err); });
+}
+useEffect(() => {
+  fetchBuyerFees();
+}, []);
+
+const getFeeDetails = (maxPrice) =>{
+ 
+  return feeDetails.length > 0 ? feeDetails
+      .filter((data)=> 
+     
+      {
+          const range = data.fee_price.replaceAll('$',"").split("-")
+         
+          if(range[1]!=="up"){
+             
+              return Number(range[0]) <= Number(maxPrice) && Number(maxPrice)  <= Number(range[1]) 
+          }
+          else{
+              return Number(range[0]) <= Number(maxPrice) 
+          }
+
+          } 
+          )[0]?.fee || 0
+      : 0
+}
+
     return (
 
       <div>
@@ -49,7 +121,8 @@ const History = () => {
                   </div>
                   
                   
-            
+            {historyDetail?.length>0? historyDetail
+            .map((historyDetail) =>   
               <div class="lotfee-inner col-lg-12">
                 <div class="row">							
                   <div class="col-lg-4">
@@ -59,23 +132,23 @@ const History = () => {
                       <p class="billsalesno">Bill Of sale# 45876</p>
                     </div>
                       <div className="historyImg">
-                        <img src={cars01} class="img-fluid" alt="..."/>
+                        <img src={historyDetail.image} class="img-fluid" alt="..."/>
                         </div>
                         <div class="cars-tag">
                         <h4>Best deal</h4>
                         </div>
                         <div class="cars-content">		
-                        <h3><a href="#">Honda amaze (2014 model)</a></h3>
+                        <h3><a href="#">{historyDetail.make} ({historyDetail.model} model)</a></h3>
                         <div class="d-flex align-items-center mb-3">
-                          <p class="details"><img src={speedometer}  alt=""/><span>31,1241 m</span></p>
+                          <p class="details"><img src={speedometer}  alt=""/><span>{historyDetail.miles} m</span></p>
                           {/* &nbsp;&nbsp;&nbsp;&nbsp; */}
-                          <p class="details"><img src={gasolinePump} alt=""/><span>Diesel</span></p>
+                          <p class="details"><img src={gasolinePump} alt=""/><span>{historyDetail.fuel_type}</span></p>
                         </div>
                         
                         
                         
                         <div class="cars-prices invoice_link">
-                          <a class="cta-btns" href="#">Inspection</a>
+                          <a class="cta-btns" href="JavaScript:void(0)" onClick={()=>redirecttoInspection(historyDetail.car_id)}>Inspection</a>
                           <a class="cta-btns invoice" href="#">Invoice</a>
                         </div>
                         <div class="cars-prices gatepass">
@@ -114,10 +187,10 @@ const History = () => {
                       
                     </form>
                     
-                    <h3>Title status - <span> Awaiting Title</span></h3>
+                      <h3>Title status - <span> {historyDetail.vehicle_type}</span></h3>
                     
                     <div class="cars-prices ">
-                      <a class="cta-btns" href="#">Price - $1900</a>
+                      <a class="cta-btns" href="#">Price - $ {historyDetail.price}</a>
                     </div>
                     
                     
@@ -130,7 +203,7 @@ const History = () => {
                     
                     <div class="vehicleimgright col-lg-12">
                       <p class="editbtn"><a class="" href="#">Edit</a></p>
-                      <h3>Vehicle Price + Lot Fee <span>$1900</span></h3>
+                      <h3>Vehicle Price + Lot Fee <span>$ {historyDetail.price}</span></h3>
                       <h4>Inspection <span>$100</span></h4>
                       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
                       
@@ -138,16 +211,17 @@ const History = () => {
                       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
                       
                       <div class="vehiclerighttotal">
-                        <h3>Total amount <span>$2,300</span></h3>
+                        <h3>Total amount <span>$ {(Number(historyDetail.price) || 0) +  100 + 100}</span></h3>
                       </div>
                     </div>
   
                   </div>
                 </div>
-              </div>
+              </div>)
+                    :""}
               
               
-              <div class="lotfee-inner col-lg-12">
+              {/* <div class="lotfee-inner col-lg-12">
                 <div class="row">							
                   <div class="col-lg-4">
                     
@@ -240,10 +314,10 @@ const History = () => {
   
                   </div>
                 </div>
-              </div>
+              </div> */}
               
               
-              <div class="lotfee-inner col-lg-12">
+              {/* <div class="lotfee-inner col-lg-12">
                 <div class="row">							
                   <div class="col-lg-4">
                     
@@ -336,7 +410,7 @@ const History = () => {
   
                   </div>
                 </div>
-              </div>
+              </div> */}
               
               
             
