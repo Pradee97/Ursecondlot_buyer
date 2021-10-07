1e44e039af9924ae16fc27bd212400ea59145082
Govind <govind@zhagaramss.com>
2021-10-08 00:30:48 +0530

    UI changes done

--- src/Pages/ManageAccount/Invoice.js
+++ src/Pages/ManageAccount/Invoice.js
@@ -290,13 +290,9 @@ const InVoice = (props) => {
    <div class="col-md-12 text-center paybtns">
    <a class={`cta-btns-primary ${(billOfSales !== null && billOfSales !== ""  ) && "greenBtn"}`} onClick={()=>{(billOfSales == null ||billOfSales == ""  )&& redirecttoCart()}} > {billOfSales !== null && billOfSales !== "" ? "paid": "pay" } </a> 
    {/* <a class={`cta-btns-primary ${(billOfSales !== null && billOfSales !== ""  ) && "redBtn"}`} onClick={()=>{(billOfSales == null ||billOfSales == ""  )&& redirecttoCart()}} > {billOfSales !== null && billOfSales !== "" ? "print": "" } </a>  */} 
-<<<<<<< HEAD
-   <button className="printBtn" type ="button" onClick= {printPage}>Print</button>
-=======
-   <div class="col-lg-12 invoiceBlock">
-   <button type ="button" onClick= {printPage}>Print</button>
->>>>>>> d2f24a8f4220ccafbf2c531268cf25b17016640b
-   </div>
+  
+   <button  className="printBtn"  type ="button" onClick= {printPage}>Print</button>
+  
    </div>
    <section id="playstoreBlock" class="playstoreBlock">
      <div class="container">
