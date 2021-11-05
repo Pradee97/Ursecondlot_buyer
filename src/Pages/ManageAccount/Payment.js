ee2bc86fb886cd520e15bd85896d5c912f96157b
rajesh <brajeshbabu2015@gmail.com>
2021-11-05 20:13:32 +0530

    Document completed

--- src/Pages/ManageAccount/Payment.js
+++ src/Pages/ManageAccount/Payment.js
@@ -254,17 +254,37 @@ const Payment = () => {
         //     setDocError("Upload Document is required")
         //     return;
         // }
+        // if(!doc)
+        // {
+        // setType("1");
+        // return;
+        // }
+        // else
+        // {
+        //     setType("");
+        // }
+        // if( type!=="1" ){
+        //     console.log("tyoe",type);
+
+            
         if(!doc)
         {
         setType("1");
+        return;
+        }
+        else if(!doc[0].type.includes('.txt') || !doc[0].type.includes('.doc') || !doc[0].type.includes('.pdf') || !doc[0].type.includes('.png') || !doc[0].type.includes('.jpeg') || !doc[0].type.includes('.jpg'))
+        {
+            setType("2");
         return;
         }
         else
         {
             setType("");
         }
+        
         if( type!=="1" ){
-            console.log("tyoe",type);
+            console.log("type",type);
+           
         API
             .post("payment_info/add", request)
             .then((response) => {
@@ -296,8 +316,8 @@ const Payment = () => {
                     setPopupActionType("close");
                     setPopupActionValue("close");
         });
-   
-        }
+    }
+        
 }
     const getFiles=(file)=>{
         //setDoc(file);
@@ -467,7 +487,7 @@ const Payment = () => {
                                                     <span className="uploadedFile">{doc.length>0?doc[0].name:doc.name}</span>
                                                     {/* <p className="form-input-error" >{docError}</p> */}
                                                     {/* {type==="0"?<p className="form-input-error">Upload only Image Format </p>:""}  */}
-                                                    {type==="1"?<p className="form-input-error">File Upload Mandatory </p>:""} 
+                                                    {type==="1"?<p className="form-input-error">File Upload Mandatory </p>:type==="2"?<p className="form-input-error">Only TXT,DOC,PDF,JPG,PNG,JPEG file formats can be uploaded</p>:""} 
                                                     </div>
                                                    
                                                     <div className="col-lg-12 loginBtn">
