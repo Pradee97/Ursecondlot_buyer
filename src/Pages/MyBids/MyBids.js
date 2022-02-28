24b6416bcd21241aea18352693a1c5b2be126748
rajesh <brajeshbabu2015@gmail.com>
2022-02-28 18:43:17 +0530

    my bids issues fixed

--- src/Pages/MyBids/MyBids.js
+++ src/Pages/MyBids/MyBids.js
@@ -291,64 +291,64 @@ const MyBids = () => {
                                 </div>
                             </div>
 
-                            {myBids.map((bidsObj) =>
+                            {myBids?.map((bidsObj) =>
                                 <div class="row mybidsdetailsblock">
                                     <div class="col-lg-3 mybidsvehicledetails" onClick={()=>{redirectpage(bidsObj.car_id,bidsObj.seller_dealer_id)}}>
                                         <div class="mybidsleftvehicle">
-                                            <img src={bidsObj.image} class="img-fluid" alt="..." />
+                                            <img src={bidsObj?.image} class="img-fluid" alt="..." />
                                         </div>
                                         <div class="mybidsrightvehicle">
-                                            <h3>{bidsObj.year} {bidsObj.make} {bidsObj.model}  </h3>
+                                            <h3>{bidsObj?.year} {bidsObj?.make} {bidsObj?.model}  </h3>
                                             <div class="d-flex align-items-center">
-                                                <p class="details"><img src={speedometer} alt="" /><span>{bidsObj.miles} m</span></p>&nbsp;&nbsp;&nbsp;&nbsp;
-                                                <p class="details"><img src={gasolinePump} alt="" /><span>{bidsObj.fuel_type}</span></p>&nbsp;&nbsp;&nbsp;&nbsp;
+                                                <p class="details"><img src={speedometer} alt="" /><span>{bidsObj?.miles} m</span></p>&nbsp;&nbsp;&nbsp;&nbsp;
+                                                <p class="details"><img src={gasolinePump} alt="" /><span>{bidsObj?.fuel_type}</span></p>&nbsp;&nbsp;&nbsp;&nbsp;
                                             </div>
-                                            <p class="details"><img src={car} alt="" /><span>{bidsObj.vin_no}</span></p>
-                                            <p class="details"><span>Inventory Number- {bidsObj.inventory_no}</span></p>
-                                            <p class="details"><span> {bidsObj.date}</span></p>
+                                            <p class="details"><img src={car} alt="" /><span>{bidsObj?.vin_no}</span></p>
+                                            <p class="details"><span>Inventory Number- {bidsObj?.inventory_no}</span></p>
+                                            <p class="details"><span> {bidsObj?.date}</span></p>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 mybidssellerdetails">
                                         <div class="mybidsleftseller">
                                             <h3>Used Car Dealer</h3>
-                                            <p class="details"><img src={Path} alt="" /><span>{bidsObj.address}</span></p>
+                                            <p class="details"><img src={Path} alt="" /><span>{bidsObj?.address}</span></p>
                                             <div class="d-flex align-items-center">
                                                 {/* <p class="details"><img src={persent} alt="" /><span>15%</span></p>&nbsp;&nbsp;&nbsp;&nbsp; */}
-                                                <p class="details"><img src={roadwithBrokenLine} alt="" /><span>{bidsObj.mileage}M</span></p>&nbsp;&nbsp;&nbsp;&nbsp;
+                                                <p class="details"><img src={roadwithBrokenLine} alt="" /><span>{bidsObj?.mileage}M</span></p>&nbsp;&nbsp;&nbsp;&nbsp;
                                             </div>
                                         </div>
                                         <div class="mybidsrightseller">
-                                            <img src={bidsObj.seller_logo} class="img-fluid" alt="..." />
+                                            <img src={bidsObj?.seller_logo} class="img-fluid" alt="..." />
                                         </div>
                                     </div>
                                     <div class="col-lg-2 mybidsbiddetails">
                                         <div class="mybidsbid">
-                                            <h4>${bidsObj.high_bid}</h4>
+                                            <h4>${bidsObj?.high_bid}</h4>
                                             {/* <p>Car seller</p> */}
-                                            {bidsObj.buyer_high_bid==bidsObj.high_bid?
+                                            {bidsObj?.buyer_high_bid==bidsObj?.high_bid?
                                                 <p>by <span>Me</span></p>:
-                                                <p>by <span>{bidsObj.high_bid_buyer_name}</span></p>
+                                                <p>by <span>{bidsObj?.high_bid_buyer_name}</span></p>
                                                 }
                                         </div>
                                     </div>
                                     <div class="col-lg-2 mybidsbuynowdetails">
                                         <div class="mybidsuynow">
-                                            {bidsObj.buy_it_now !==""?
-                                            <h4>$ {bidsObj.buy_it_now}</h4>:""}
+                                            {bidsObj?.buy_it_now !==""?
+                                            <h4>$ {bidsObj?.buy_it_now}</h4>:""}
                                         </div>
                                     </div>
                                     <div class="col-lg-2 mybidscontroldetails">
-                                    {(bidsObj.isbuyercounterbid=="me" && bidsObj.iscounterbid!==null && (bidsObj.time !==0 || bidsObj.time!==null)) || ((bidsObj.iscounterbid==null || bidsObj.iscounterbid=="no" ) && (bidsObj.isbuyercounterbid==null || bidsObj.isbuyercounterbid=="not")&&(bidsObj.time ==0 || bidsObj.time==null))?
+                                    {(bidsObj?.isbuyercounterbid=="me" && bidsObj?.iscounterbid!==null && (bidsObj?.time !==0 || bidsObj?.time!==null)) || ((bidsObj?.iscounterbid==null || bidsObj.iscounterbid=="no" ) && (bidsObj.isbuyercounterbid==null || bidsObj.isbuyercounterbid=="not")&&(bidsObj.time ==0 || bidsObj.time==null))?
                                         <div class="mybidscontrol">
                                             <a className={`${lateFeeValue > 0 && 'buy-it-disable-btn'} cta-btns-primary redBtn`}  href="JavaScript:void(0)" onClick={() => setMakeBitValue(bidsObj.high_bid, bidsObj.min_price, bidsObj.save_purchase, bidsObj.car_id, bidsObj.time, bidsObj.counter_buyer_dealer_id, bidsObj.max_price, bidsObj.buy_it_now, bidsObj.comments, bidsObj.transportation, bidsObj.display, bidsObj.proxy_bid, bidsObj.transportation_charge, bidsObj.save_policy,bidsObj.credit_limit,bidsObj.lot_fee,bidsObj.image,bidsObj.model,bidsObj.make,bidsObj.year)}>Raise Bid</a>
-                                            {bidsObj.buy_it_now !==""?
+                                            {bidsObj?.buy_it_now !==""?
                                             <a className={`${lateFeeValue > 0 && 'buy-it-disable-btn'} cta-btns greenBtn`} href="JavaScript:void(0)" onClick={()=>setBuyItNowValue(bidsObj.buy_it_now,bidsObj.car_id,bidsObj.image,bidsObj.model,bidsObj.make,bidsObj.year,bidsObj.price,bidsObj.transportation,bidsObj.transportation_charge,bidsObj.lot_fee,bidsObj.credit_limit)} >Accept Bid</a>:""}
-                                            {(bidsObj.cancel_bid_time!==null && bidsObj.cancel_bid_time!==0 && bidsObj.cancel_bid_time < 400) ?
+                                            {(bidsObj?.cancel_bid_time!==null && bidsObj?.cancel_bid_time!==0 && bidsObj?.cancel_bid_time < 400) ?
 									 
                                      (<div>
                                           <a class="control-btns-cancel" disabled >Cancel Bid Activate After Some Times</a>
-                                         <div class= {(bidsObj.cancel_bid_time!==null && bidsObj.cancel_bid_time!==0 && bidsObj.cancel_bid_time < 400)?"countownBlock":""} >
-                                         <Countdown date={Date.now() + (bidsObj.cancel_bid_time!==null && bidsObj.cancel_bid_time < 400 ? bidsObj.cancel_bid_time*60*1000 :0)  } renderer={render} />
+                                         <div class= {(bidsObj?.cancel_bid_time!==null && bidsObj?.cancel_bid_time!==0 && bidsObj?.cancel_bid_time < 400)?"countownBlock":""} >
+                                         <Countdown date={Date.now() + (bidsObj?.cancel_bid_time!==null && bidsObj?.cancel_bid_time < 400 ? bidsObj?.cancel_bid_time*60*1000 :0)  } renderer={render} />
                                         
                                          </div>
                                     
@@ -361,27 +361,27 @@ const MyBids = () => {
                                         </div>:
                                          <div class="mybidscontrol">
                                          <a class="control-btns-cancel" >Locked up for Higher Bid</a>
-                                         {(bidsObj.cancel_bid_time!==null && bidsObj.cancel_bid_time!==0 && bidsObj.cancel_bid_time < 300) ?
+                                         {(bidsObj?.cancel_bid_time!==null && bidsObj?.cancel_bid_time!==0 && bidsObj?.cancel_bid_time < 300) ?
 									 
                                      (<div>
                                           <a class="control-btns-cancel" diabled >Cancel Bid Activate After Some Times</a>
-                                         <div class= {(bidsObj.cancel_bid_time!==null && bidsObj.cancel_bid_time!==0 && bidsObj.cancel_bid_time < 300)?"countownBlock":""} >
-                                         <Countdown date={Date.now() + (bidsObj.cancel_bid_time!==null && bidsObj.cancel_bid_time < 300 ? bidsObj.cancel_bid_time*60*1000 :0)  } renderer={render} />
+                                         <div class= {(bidsObj?.cancel_bid_time!==null && bidsObj?.cancel_bid_time!==0 && bidsObj?.cancel_bid_time < 300)?"countownBlock":""} >
+                                         <Countdown date={Date.now() + (bidsObj?.cancel_bid_time!==null && bidsObj?.cancel_bid_time < 300 ? bidsObj?.cancel_bid_time*60*1000 :0)  } renderer={render} />
                                         
                                          </div>
                                     
                                      </div>
                                      ):
                                      (<div>
-                                     <a class="control-btns-cancel" onClick={()=>setCancelBidValue(bidsObj.car_id,bidsObj.image,bidsObj.model,bidsObj.make,bidsObj.year)}>Cancel Bid</a></div>)
+                                     <a class="control-btns-cancel" onClick={()=>setCancelBidValue(bidsObj?.car_id,bidsObj?.image,bidsObj?.model,bidsObj?.make,bidsObj?.year)}>Cancel Bid</a></div>)
 
                                      } 
                                      </div>}
                                      
-                                        {(bidsObj.buyer_high_bid==bidsObj.high_bid || bidsObj.buyer_high_bid!==bidsObj.high_bid) &&
+                                        {(bidsObj?.buyer_high_bid==bidsObj?.high_bid || bidsObj?.buyer_high_bid!==bidsObj?.high_bid) &&
 									 
-                                            <div class= {(bidsObj.time!==null && bidsObj.time < 20)?"countownBlock":""} >
-                                                <Countdown date={Date.now() + (bidsObj.time!==null && bidsObj.time < 20 ? bidsObj.time*60*1000 :0)  } renderer={renderer} />
+                                            <div class= {(bidsObj?.time!==null && bidsObj?.time < 20)?"countownBlock":""} >
+                                                <Countdown date={Date.now() + (bidsObj?.time!==null && bidsObj?.time < 20 ? bidsObj?.time*60*1000 :0)  } renderer={renderer} />
                                             
                                             </div>}
 
